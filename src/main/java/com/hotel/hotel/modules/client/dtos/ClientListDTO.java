6107240c462cae769d793978c881876933433b05
Peterson William <petersondev.profissional@gmail.com>
2026-09-06 22:38:34 -0300

    fix: ajustando helper de segurança

--- src/main/java/com/hotel/hotel/modules/client/dtos/ClientListDTO.java
+++ src/main/java/com/hotel/hotel/modules/client/dtos/ClientListDTO.java
@@ -4,9 +4,10 @@ import java.time.LocalDate;
 
 import com.hotel.hotel.modules.client.model.Client;
 import com.hotel.hotel.modules.contactInformation.dtos.ContactInformationDTO;
+import com.hotel.hotel.modules.user.dtos.UserResponseDTO;
 
-public record ClientListDTO(Long id, String name, String email, LocalDate dateOfBirth, ContactInformationDTO contactInformation) {
+public record ClientListDTO(Long id, String name, String email, String pin, LocalDate dateOfBirth, ContactInformationDTO contactInformation, UserResponseDTO user) {
     public ClientListDTO(Client client) {
-        this(client.getId(), client.getName(), client.getEmail(), client.getDateOfBirth(), new ContactInformationDTO(client.getContactInformation()));
+        this(client.getId(), client.getName(), client.getEmail(), client.getPin(), client.getDateOfBirth(), new ContactInformationDTO(client.getContactInformation()), new UserResponseDTO(client.getUser()));
     }
 }
